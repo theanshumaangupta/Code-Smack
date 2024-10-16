1540ebf25900ef66df5094ac8b2c233448d85b93
Anshumaan Gupta <theanshumangupta@gmail.com>
2024-10-16 09:42:32 +0530

    updating packages

--- apps/frontend/db/index.ts
+++ apps/frontend/db/index.ts
@@ -1,18 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+// Load environment variables from .env file
 dotenv.config();
+
 const prismaClientSingleton = () => {
-    return new PrismaClient();
+	return new PrismaClient().$extends(withAccelerate());
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
+// Declare a global type for Prisma to use in development to avoid multiple instances
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClientSingleton | undefined;
+	prisma: PrismaClientSingleton | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+// Create the `prisma` instance based on the environment
+let prisma: PrismaClientSingleton;
+
+if (process.env.NODE_ENV === "production") {
+	// In production, create a new PrismaClient for each invocation
+	prisma = prismaClientSingleton();
+} else {
+	// In development, reuse the existing instance attached to globalThis
+	if (!globalForPrisma.prisma) {
+		globalForPrisma.prisma = prismaClientSingleton();
+	}
+	prisma = globalForPrisma.prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
